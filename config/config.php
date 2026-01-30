b745c33fb95a85824aa774826fb7ff392e0ae756
RamerKlipel <ramerh@outlook.com>
2026-01-30 00:42:12 -0300

    feat: ajuste no tratameto do config

--- config/config.php
+++ config/config.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once __DIR__ . '/control.php';
 
 $conn = driver .':host='. host .';dbname='. dbname;
@@ -8,5 +8,5 @@ $user = trim(user);
 try {
     $stmt = new PDO($conn, $user, $password);
 } catch (PDOException $e) {
-    throw new PDOException ($e->getMessage(), $e->getCode());
+    echo 'erro ao conectar ao banco de dados: ' .$e->getMessage(). ' arquivo: ' .$e->getFile(). ' linha: ' .$e->getLine(). ' Código do erro: ', $e->getCode();die;
 }
