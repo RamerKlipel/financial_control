428bf448e4789146396a0057267ce00437bc7339
RamerKlipel <ramerh@outlook.com>
2026-03-12 23:57:47 -0300

    feat: new modal alert function

--- public/js/pageForm.js
+++ public/js/pageForm.js
@@ -1,3 +1,4 @@
+import mountModalAlert from "../utilities/mountmodalalert.js";
 class pageForm {
     constructor() {
         this.elmtForm = document.querySelector('form');
@@ -9,6 +10,59 @@ class pageForm {
             e.preventDefault();
             this.submit();
         });
+
+        document.querySelectorAll('.btn-action[data-action]').forEach(elmt => {
+            elmt.addEventListener('click', (e) => {
+                const action = elmt.getAttribute('data-action');
+                const href = elmt.getAttribute('href');
+                if (action == "d") {
+                    e.preventDefault();
+
+                    mountModalAlert({
+                        'width': '50%',
+                        'title': 'Attention',
+                        'body': 'you sure you want delete this?',
+                        'icon': 'warning',
+                        onConfirm:() => {
+                            fetch(href, {
+                                method: 'get'
+                            })
+                            .then(res => {
+                                if (!res.ok) {
+                                    throw new Error('server error ' + res.status);
+                                }
+                                mountModalAlert({
+                                    'body': 'the item was deleted successfully!',
+                                    'icon': 'success',
+                                    'showCancelBtn' : false,
+                                    'confirmBtnText': 'Ok',
+                                    onConfirm: () => {
+                                        window.location.reload();
+                                    }
+                                })
+                            })
+                            .catch(res => {
+                                console.log(res);
+                                mountModalAlert({
+                                    'body': res,
+                                    'icon': 'error',
+                                    'showCancelBtn' : false,
+                                    'confirmBtnText': 'Ok'
+                                })
+                            })
+                        }
+                    })
+                }
+            });
+        });
+        // .forEach((btn) => {
+        //     btn.addEventListener('click', (a) => {
+        //         a.preventDefault();
+        //         switch (action) {
+        //             case d
+        //         }
+        //     })
+        // });
     }
 
     submit() {
@@ -19,7 +73,8 @@ class pageForm {
             body: formData
         })
         .then(res => {
-            console.log({'res': res})
+            this.preventDefault();
+            window.location.href = url+'?complete=true';
         })
         .catch(err => {
             if (err) console.log(err);
