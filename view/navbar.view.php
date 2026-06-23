03c0fdda83d17c3fcedaf5f3127cf7bd08caaf31
Ramer <ramer@hgv.inf.br>
2026-06-23 17:16:15 -0300

    feat: fix depencencies and ai message article fixed

--- view/navbar.view.php
+++ view/navbar.view.php
@@ -1,4 +1,4 @@
-<base href="/controle_financeiro/">
+<base href="/financial_control/">
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
         <a class="navbar-brand" href="index">Financial Control</a>
